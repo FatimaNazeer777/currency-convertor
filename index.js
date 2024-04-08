55840adb9aa0f477582ef714ecd5236e56c304c3
Fatima Nazeer <147838099+FatimaNazeer777@users.noreply.github.com>
2024-04-08 20:53:53 +0500

    I created Currency Convertor

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1, //Base Currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    AED: 3.67,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: chalk.magenta("Enter from Currency"),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "AED"]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: chalk.blue("Enter from Currency"),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "AED"]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: chalk.yellow("Enter your Amount")
+    }
+]);
+let fromAmount = currency[user_answer.from]; //Exchange Rate
+let toAmount = currency[user_answer.to]; //Exchange Rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency
+let convertedAmount = Math.floor(baseAmount * toAmount);
+console.log(chalk.greenBright(`Your ${user_answer.from} ${user_answer.amount} is equal to ${user_answer.to} ${convertedAmount}`));
